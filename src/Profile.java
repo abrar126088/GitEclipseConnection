3dd113b373f638aa0e6bd390167358e66884771c
Abrar <97743670+Abrarkhan126088@users.noreply.github.com>
2026-09-24 09:24:33 +0530

    added profile and homeclass

---
+++ src/Profile.java
@@ -0,0 +1,18 @@
+
+public class Profile {
+	
+	public void validProfile() {
+		
+		System.out.println("title has been verified");
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println();
+
+	}
+
+}
